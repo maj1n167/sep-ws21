31d312ca942d12ede918330c98f470755e7b8684
Oguzhan Kaluk <oguzhan.kaluk@stud.uni-due.de>
2021-10-21 15:48:34 +0200

    Die Basis hochladen

---
+++ src/main/java/sep/gruppe12/SupremeEatingProgramApplication.java
@@ -0,0 +1,13 @@
+package sep.gruppe12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupremeEatingProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupremeEatingProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sep/gruppe12/SupremeEatingProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package sep.gruppe12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupremeEatingProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
