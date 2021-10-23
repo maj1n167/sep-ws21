60c06fcdf0d8c4b46d2ce5f86c0d6836d49651dc
manaog <oguzhan.kaluk@gmail.com>
2021-10-23 19:29:26 +0200

    added RestaurantController and RestaurantService

--- server/src/main/java/com/example/server/controller/RestaurantController.java
+++ server/src/main/java/com/example/server/controller/RestaurantController.java
@@ -1,4 +1,43 @@
 package com.example.server.controller;
 
-public class RestaurantController {
-}
+import com.example.server.model.Restaurant;
+import com.example.server.service.RestaurantService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+    @RestController
+    @RequestMapping("/restaurant")
+    public class RestaurantController {
+        private final RestaurantService restaurantService;
+
+
+        public RestaurantController(RestaurantService restaurantService) {
+            this.restaurantService = restaurantService;
+        }
+
+
+        @GetMapping
+        public ResponseEntity<List<Restaurant>> getAllRestaurants() {
+
+            List<Restaurant> restaurants = restaurantService.findAllRestaurants();
+
+            return new ResponseEntity<>(restaurants, HttpStatus.OK);
+
+        }
+
+        @PostMapping("/add")
+        public ResponseEntity<Restaurant> addRestaurant(@RequestBody Restaurant restaurants) {
+            Restaurant newRestaurant = restaurantService.addRestaurant(restaurants);
+            return new ResponseEntity<>(newRestaurant, HttpStatus.CREATED);
+        }
+
+        @PutMapping("/update")
+        public ResponseEntity<Restaurant> updateRestaurant(@RequestBody Restaurant restaurants) {
+            Restaurant updateRestaurant = restaurantService.addRestaurant(restaurants);
+            return new ResponseEntity<>(updateRestaurant, HttpStatus.OK);
+        }
+    }
+
