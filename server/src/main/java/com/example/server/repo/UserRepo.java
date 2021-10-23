7bdfb68e6f7ae45a2863a5c855720e071901df62
manaog <oguzhan.kaluk@gmail.com>
2021-10-23 21:00:38 +0200

    einige anpassungen email PK -> id PK

--- server/src/main/java/com/example/server/repo/UserRepo.java
+++ server/src/main/java/com/example/server/repo/UserRepo.java
@@ -3,6 +3,6 @@ package com.example.server.repo;
 import com.example.server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User,String> {
+public interface UserRepo extends JpaRepository<User, Integer> {
 
 }
