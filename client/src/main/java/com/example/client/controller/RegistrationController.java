185828af610b1bcaf3336d863fc365884229d2f6
Leon Gashi <leon.gashi@stud.uni-due.de>
2021-10-25 15:48:21 +0200

    Upload von FXML dateien für login und registrierung.

--- client/src/main/java/com/example/client/controller/RegistrationController.java
+++ client/src/main/java/com/example/client/controller/RegistrationController.java
@@ -1,4 +1,34 @@
 package com.example.client.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
 public class RegistrationController {
+
+    @FXML
+    Button RegisterButton;
+    @FXML
+    TextField vornameTextfield;
+    @FXML
+    TextField nameTextfield;
+    @FXML
+    TextField emailTextfield;
+    @FXML
+    PasswordField passwortTextfield;
+    @FXML
+    TextField straßeTextfield;
+    @FXML
+    TextField postleitzahlTextfield;
+    @FXML
+    TextField stadtTextfield;
+
+    @FXML
+    protected void registerButtonClick() {
+
+
+
+    }
+
 }
