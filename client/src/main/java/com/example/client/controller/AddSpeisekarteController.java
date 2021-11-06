9746357f0fd766f6ccc9f80b9a8b861b913664b9
DursunBerat <dursun.saral@stud.uni-due.de>
2021-11-06 16:38:21 +0100

    Speisekarte

--- client/src/main/java/com/example/client/controller/AddSpeisekarteController.java
+++ client/src/main/java/com/example/client/controller/AddSpeisekarteController.java
@@ -1,9 +1,11 @@
 package com.example.client.controller;
 
+import com.example.client.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.io.IOException;
 
 public class AddSpeisekarteController {
     @FXML
@@ -15,12 +17,45 @@ public class AddSpeisekarteController {
     @FXML
     private Button dateiUrl;
     @FXML
-    private ComboBox kategorie;
+    private ChoiceBox <String> kategorie;
     @FXML
     private TextField name;
     @FXML
     private TextField beschreibung;
     @FXML
     private TextField preis;
+
+
+    public void auswahl(){
+        kategorie.getItems().addAll("Pizza","Pasta","Salate","Desserts");
+        kategorie.setValue("Pizza");
+    }
+
+    @FXML
+    public void speichernClick() {
+        if (name.getText().equals("")||beschreibung.getText().equals("")||preis.getText().equals("")){
+
+            Alert alert = new Alert (Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setTitle("Error: Fehlende Zeile");
+            alert.setContentText("Bitte füllen Sie alle Felder aus");
+
+            alert.showAndWait();
+        }
+        else {
+
+
+        }
+
+    }
+
+    @FXML
+    public void speichernClick (ActionEvent event) throws IOException {
+        // Change Scenes
+        Main m= new Main();
+       // soll zur Speisekarte zurück
+        m.ChangeScene("CreateMenu.fxml");
+
+    }
 
 }
