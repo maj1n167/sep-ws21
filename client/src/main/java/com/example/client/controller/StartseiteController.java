64aa07e7185bc61729d0275d7167c2d9ed1d94d2
Leon Gashi <leon.gashi@stud.uni-due.de>
2021-11-07 22:40:34 +0100

    Verbesserung StartseiteFxml und StartseiteController

--- client/src/main/java/com/example/client/controller/StartseiteController.java
+++ client/src/main/java/com/example/client/controller/StartseiteController.java
@@ -1,4 +1,28 @@
 package com.example.client.controller;
 
+import com.example.client.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class StartseiteController {
+    public void onEditProfileClick(ActionEvent event) {
+    }
+
+    public void onCreateRestaurantClick(ActionEvent event) {
+    }
+
+    public void onEditRestaurantClick(ActionEvent event) {
+    }
+
+    public void onSpeisekarteClick(ActionEvent event) {
+    }
+
+    @FXML
+    public void onLogoutClick(ActionEvent event) throws IOException {
+        // Change Scenes
+        Main m= new Main();
+        m.ChangeScene("Login.fxml");
+    }
 }
