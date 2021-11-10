168c485df75cf001d5bd98be7061ba6a59558e75
DursunBerat <dursun.saral@stud.uni-due.de>
2021-11-10 22:59:15 +0100

    EditFood % EditFoodController update

--- client/src/main/java/com/example/client/controller/EditFoodController.java
+++ client/src/main/java/com/example/client/controller/EditFoodController.java
@@ -1,34 +1,46 @@
 package com.example.client.controller;
 
 import com.example.client.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class EditFoodController extends ConnectionController {
-
     @FXML
-    private Button bearbeiten;
+    private Button deleteMenu;
+    @FXML
+    private Button deleteFood;
     @FXML
     private Button speichern;
     @FXML
-    private Button dateiXml;
+    private TextArea AusgabeSpeisekarte;
     @FXML
-    private Button dateiUrl;
+    private TextField  foodId;
     @FXML
-    private ChoiceBox<String> kategorie;
+    private ChoiceBox<String> neuekategorie;
     @FXML
-    private TextField name;
+    private TextField neuername;
     @FXML
-    private TextField beschreibung;
+    private TextField neuebeschreibung;
     @FXML
-    private TextField preis;
+    private TextField neuepreis;
+
+
+   public void initialize() throws IOException{
+
+
+
+   }
 
     @FXML
     public void änderungSpeichern()throws IOException {
+// hier werden die alten daten von den neuen überschrieben
+            String url = "http://localhost:8080/food/update";
 
     }
 
@@ -39,5 +51,20 @@ public class EditFoodController extends ConnectionController {
     public void zurückButton() throws IOException{
         Main m = new Main();
         m.ChangeScene("Speisekarte.fxml");
+    }
+
+    public void deleteMenu() throws IOException {
+
+
+    }
+
+    public void deleteFood() throws IOException {
+       
+
+    }
+
+    public void fertigButton() throws IOException{
+
+
     }
 }
