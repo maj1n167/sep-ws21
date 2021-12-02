25ee9c4c424a0935b78a9a0a43df0e8183a91fc8
Leon Gashi <leon.gashi@stud.uni-due.de>
2021-12-02 11:16:30 +0100

    TreuepunkteController und TreuepunkteFxml hinzugefügt

---
+++ client/src/main/java/com/example/client/controller/TreuepunkteController.java
@@ -0,0 +1,25 @@
+package com.example.client.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class TreuepunkteController extends ConnectionController {
+
+    @FXML
+    TextField DiscountText;
+
+    @FXML
+    public void onDiscountButtonClick(ActionEvent event) {
+
+
+    }
+
+    @FXML
+    public void onGoBackButtonClick(ActionEvent event) {
+
+
+    }
+
+
+}
