4eb02a89ad19a8b3422ae5878b0274dd8cbfc7ed
DursunBerat <dursun.saral@stud.uni-due.de>
2021-12-05 15:00:52 +0100

    Backend BestellController & BestellHisController und BestellService & BestellHisService

--- server/src/main/java/com/example/server/controller/BestellController.java
+++ server/src/main/java/com/example/server/controller/BestellController.java
@@ -1,4 +1,40 @@
 package com.example.server.controller;
 
+import com.example.server.model.Bestellungen;
+import com.example.server.service.BestellService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Component
+@RestController
+@RequestMapping("/bestell")
+
 public class BestellController {
+    private final BestellService bestellService;
+
+
+    public BestellController(BestellService bestellService){this.bestellService = bestellService;}
+
+    @GetMapping
+    public ResponseEntity<List<Bestellungen>> getAllBestellungens() {
+        List<Bestellungen> bestellungen = bestellService.findAllBestellungens();
+        return new ResponseEntity<>(bestellungen, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Bestellungen> addUser(@RequestBody Bestellungen bestellungen) {
+        Bestellungen newBestellungen = bestellService.addBestellungen(bestellungen);
+        return new ResponseEntity<>(newBestellungen, HttpStatus.CREATED);
+    }
+    @DeleteMapping("/delete/{bestellId}")
+    public ResponseEntity deleteBestellungen(@PathVariable("bestellId") int bestellId) {
+        bestellService.deleteBestellungen(bestellId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
--- server/src/main/java/com/example/server/controller/BestellHisController.java
+++ server/src/main/java/com/example/server/controller/BestellHisController.java
@@ -1,4 +1,35 @@
 package com.example.server.controller;
 
+import com.example.server.model.BestellHistorie;
+import com.example.server.service.BestellHisService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Component
+@RestController
+@RequestMapping
 public class BestellHisController {
+    private final BestellHisService bestellHisService;
+
+    public BestellHisController(BestellHisService bestellHisService) {this.bestellHisService = bestellHisService;}
+
+    @GetMapping
+    public ResponseEntity <List<BestellHistorie>>getAllBestellhistorie(){
+       List<BestellHistorie>bestellHistories= bestellHisService.findAllBestellHistories();
+       return new ResponseEntity<>(bestellHistories,HttpStatus.OK);}
+
+    @PostMapping("/add")
+        public ResponseEntity<BestellHistorie> addUser(@RequestBody BestellHistorie bestellHistorie) {
+       BestellHistorie newBestellHistorie = bestellHisService.addBestellhistorie(bestellHistorie);
+        return new ResponseEntity<>(newBestellHistorie, HttpStatus.CREATED);
+   }
+    @DeleteMapping("/delete/{bestellHisId}")
+    public ResponseEntity deleteBestellHistorie(@PathVariable("bestellHisId") int bestellHisId) {
+        bestellHisService.deleteBestellHistorie(bestellHisId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
