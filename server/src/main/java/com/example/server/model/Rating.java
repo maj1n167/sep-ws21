52eba8ba8252c5ef868b95b22dc63b9871b4c5fc
Oguzhan Kaluk <oguzhan.kaluk@stud.uni-due.de>
2021-12-06 15:03:32 +0100

    Backend Rating hinzugefuegt

--- server/src/main/java/com/example/server/model/Rating.java
+++ server/src/main/java/com/example/server/model/Rating.java
@@ -1,11 +1,14 @@
 package com.example.server.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Rating {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     int starsLieferung;
     int starsFood;
