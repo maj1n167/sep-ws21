87392f05d088c8ec39466b8537f8f611d8dcc597
Leon Gashi <leon.gashi@stud.uni-due.de>
2021-12-10 21:28:20 +0100

    Treuepunkte-Update: Man kann 1 Treuepunkt per Knopfdruck generieren und bei 10 Treuepunkten wird automatisch ein Coupon an email gesendet

--- client/src/main/java/com/example/client/controller/TreuepunkteController.java
+++ client/src/main/java/com/example/client/controller/TreuepunkteController.java
@@ -3,7 +3,10 @@ package com.example.client.controller;
 import com.example.client.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -11,6 +14,10 @@ public class TreuepunkteController extends ConnectionController {
 
     @FXML
     TextField DiscountText;
+
+    @FXML
+    public int userId;
+    public int treuepunkte;
 
     @FXML
     public void onDiscountButtonClick(ActionEvent event) {
@@ -32,6 +39,72 @@ public class TreuepunkteController extends ConnectionController {
         Main m = new Main();
         m.ChangeScene("KStartseite.fxml");
     }
+
+    public void initialize() throws IOException{
+        userId = LoginController.userId;
+        String url1 = "http://localhost:8080/user";
+        JSONArray jsonArray =  new JSONArray(JSONObjectGET(url1).toString());
+        JSONObject jsonObject = new JSONObject();
+
+        for(int i = 0; i<jsonArray.length(); i++){
+            JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+
+            if (jsonObject1.get("userId").equals(userId)){
+                jsonObject = jsonObject1;
+            }
+        }
+        treuepunkte=Integer.parseInt(jsonObject.get("treuepunkte").toString());
+        if(treuepunkte >= 10) {
+
+            String url2 ="http://localhost:8080/coupon/send/";
+
+            JSONObjectGET(url2 + LoginController.email);
+
+            treuepunkte = treuepunkte - 10;
+            String url = "http://localhost:8080/user";
+            JSONArray jsonArray1 = new JSONArray(JSONObjectGET(url).toString());
+            JSONObject jsonObjectx = new JSONObject();
+
+            for (int i = 0; i < jsonArray1.length(); i++) {
+                JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+
+                if (jsonObject1.get("userId").equals(userId)) {
+                    jsonObjectx = jsonObject1;
+                }
+            }
+            jsonObject.put("treuepunkte", treuepunkte);
+            JSONObjectPOST("http://localhost:8080/user/add", jsonObjectx.toString());
+            initialize();
+
+        }
+    }
+
+    @FXML
+    public void onAddTreuepunkt(ActionEvent event) throws IOException {
+
+
+            int EinTreuepunkt = 0;
+            EinTreuepunkt = EinTreuepunkt + 1;
+            treuepunkte += EinTreuepunkt;
+            String url = "http://localhost:8080/user";
+            JSONArray jsonArray = new JSONArray(JSONObjectGET(url).toString());
+            JSONObject jsonObject = new JSONObject();
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+
+                if (jsonObject1.get("userId").equals(userId)) {
+                    jsonObject = jsonObject1;
+                }
+            }
+            jsonObject.put("treuepunkte", treuepunkte);
+            JSONObjectPOST("http://localhost:8080/user/add", jsonObject.toString());
+            initialize();
+
+        }
+
+
+
 
 
 }
