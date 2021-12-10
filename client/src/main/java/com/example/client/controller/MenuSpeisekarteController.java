0bcd213d134e79893e3eafbe5673a733e7f4caed
DursunBerat <dursun.saral@stud.uni-due.de>
2021-12-10 19:49:50 +0100

    Menu

--- client/src/main/java/com/example/client/controller/MenuSpeisekarteController.java
+++ client/src/main/java/com/example/client/controller/MenuSpeisekarteController.java
@@ -1,33 +1,75 @@
 package com.example.client.controller;
 
 import com.example.client.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
 
-public class MenuSpeisekarteController extends ConnectionController{
+public class MenuSpeisekarteController extends ConnectionController {
     @FXML
-    ListView<String> listView;
+    TableView list = new TableView();
+    @FXML
+    TableColumn Kategorie = new TableColumn("Kategorie");
+    @FXML
+    TableColumn AddButton = new TableColumn("zumWarenkorb");
+    @FXML
+    TableColumn Preis = new TableColumn("Preis");
+    @FXML
+    TableColumn Beschreibung = new TableColumn("Beschreibung");
+    @FXML
+    TableColumn Id = new TableColumn("Id");
+    @FXML
+    TableColumn Bild = new TableColumn("Bild");
+    @FXML
+    TableColumn Name = new TableColumn("Name");
+    private ObservableList<Menu> data = FXCollections.observableArrayList();
 
-    public void initialize(){
-        listView.getItems().addAll("Pizza","Döner");
-        //listView.getItems().setAll("Pizza","Döner","Spaghetti");
-        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
-
+    public void initialize() {
+        try {
+            list.setEditable(true);
+            Bild.setCellValueFactory(new PropertyValueFactory<Menu, String>("Bild"));
+            //Kategorie.setCellValueFactory(new PropertyValueFactory<Menu, String>("Kategorie"));
+            Name.setCellValueFactory(new PropertyValueFactory<Menu, String>("Name"));
+            Beschreibung.setCellValueFactory(new PropertyValueFactory<Menu, String>("Beschreibung"));
+            Preis.setCellValueFactory(new PropertyValueFactory<Menu, String>("Kategorie"));
+            Id.setCellValueFactory(new PropertyValueFactory<Menu, String>("Id"));
+            list.setItems(getSpeisekarte());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    public void WarenkorbAdd(){
-       String x = listView.getSelectionModel().getSelectedItems().toString();
-        System.out.println(x);
+
+    public ObservableList<Menu> getSpeisekarte() throws IOException {
+        ObservableList<Menu> output = FXCollections.observableArrayList();
+        JSONArray j = new JSONArray(JSONObjectGET("http://localhost:8080/Menu").toString());
+        for (int i = 0; i < j.length(); i++) {
+
+            JSONObject current = new JSONObject(j.get(i).toString());
+            Menu r = new Menu();
+            output.add(r = new Menu(current.getString("Bild"), current.getString("Name"),
+                    current.getString("Beschreibung"), current.getString("Preis"), current.getDouble("Id");
+        }
+        data= FXCollections.observableArrayList(output);
+        return output;
+
+            public void zurückButton () throws IOException {
+                Main m = new Main();
+                m.ChangeScene("KStartseite");
+            }
 
 
-    }
-
-    public void zurückButton() throws IOException {
-        Main m = new Main();
-        m.ChangeScene("KStartseite");
+        }
     }
 }
