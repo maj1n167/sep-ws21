6c70f915f755065d28116dd78007f5a0044b32e3
Leon Gashi <leon.gashi@stud.uni-due.de>
2021-12-14 20:23:20 +0100

    12345

--- client/src/main/java/ude/sep/client/controller/WarenkorbController.java
+++ client/src/main/java/ude/sep/client/controller/WarenkorbController.java
@@ -121,36 +121,6 @@ public class WarenkorbController extends ConnectionController {
 
         if (Double.parseDouble(user.get("guthaben").toString()) >= Double.parseDouble(jsonObject.get("summe").toString()) &&
                 Double.parseDouble(jsonObject.get("summe").toString()) >= Double.parseDouble(restaurant.get("mbw").toString())) {
-
-            //Treuepunkte Anfang - LG
-
-            initialize2();
-            JSONObject jsonObject31 = new JSONObject(JSONObjectGET("http://localhost:8080/warenkorb/find/" + userId).toString());
-            profuenfer = Double.parseDouble(jsonObject31.get("summe").toString());
-            while (profuenfer >= 5) {
-                int EinTreuepunkt = 0;
-                EinTreuepunkt = EinTreuepunkt + 1;
-                treuepunkte += EinTreuepunkt;
-                String url = "http://localhost:8080/user";
-                JSONArray jsonArrayABC = new JSONArray(JSONObjectGET(url).toString());
-                JSONObject jsonObjectABC = new JSONObject();
-
-                for (int i = 0; i < jsonArrayABC.length(); i++) {
-                    JSONObject jsonObjectXYZ = jsonArrayABC.getJSONObject(i);
-
-                    if (jsonObjectXYZ.get("userId").equals(userId)) {
-                        jsonObjectABC = jsonObjectXYZ;
-                    }
-                }
-                jsonObjectABC.put("treuepunkte", treuepunkte);
-                JSONObjectPOST("http://localhost:8080/user/add", jsonObjectABC.toString());
-                profuenfer = profuenfer - 5;
-                initialize2();
-            }
-
-            //Treuepunkte Ende - LG
-
-
             double neuesGuthaben = Double.parseDouble(user.get("guthaben").toString()) - Double.parseDouble(jsonObject.get("summe").toString());
             user.put("guthaben", neuesGuthaben);
             JSONObjectPOST("http://localhost:8080/user/add", user.toString());
@@ -208,6 +178,35 @@ public class WarenkorbController extends ConnectionController {
                 }
             }
             System.out.println(bestellung);
+
+
+            //Treuepunkte Anfang - LG
+
+            initialize2();
+            JSONObject jsonObject31 = new JSONObject(JSONObjectGET("http://localhost:8080/warenkorb/find/" + userId).toString());
+            profuenfer = Double.parseDouble(jsonObject31.get("summe").toString());
+            while (profuenfer >= 5) {
+                int EinTreuepunkt = 0;
+                EinTreuepunkt = EinTreuepunkt + 1;
+                treuepunkte += EinTreuepunkt;
+                String url = "http://localhost:8080/user";
+                JSONArray jsonArrayABC = new JSONArray(JSONObjectGET(url).toString());
+                JSONObject jsonObjectABC = new JSONObject();
+
+                for (int i = 0; i < jsonArrayABC.length(); i++) {
+                    JSONObject jsonObjectXYZ = jsonArrayABC.getJSONObject(i);
+
+                    if (jsonObjectXYZ.get("userId").equals(userId)) {
+                        jsonObjectABC = jsonObjectXYZ;
+                    }
+                }
+                jsonObjectABC.put("treuepunkte", treuepunkte);
+                JSONObjectPOST("http://localhost:8080/user/add", jsonObjectABC.toString());
+                profuenfer = profuenfer - 5;
+                initialize2();
+            }
+
+            //Treuepunkte Ende - LG
 
 
             JSONObject jsonObject2 = new JSONObject();
