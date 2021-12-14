3a4323c40a0fb3b9b456b3bd415b7b8937906b4e
nicklas czorny <nicklas.czorny@stud.uni-due.de>
2021-12-14 21:12:24 +0100

    Test der BestellHisRepo Update

--- server/src/test/java/ude/sep/repo/BestellHisRepoTest.java
+++ server/src/test/java/ude/sep/repo/BestellHisRepoTest.java
@@ -1,6 +1,9 @@
 package ude.sep.repo;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +28,25 @@ class BestellHisRepoTest {
 
     @BeforeTestClass
     public void createBestellFood() {
-        bestellFoodInto = new BestellFood(1, 1, 10.0,
+        //Essen erstellen
+        BestellFood bestellFoodInto1 = new BestellFood(1, 1, 10.0,
                 "Big Mac", "Mit Cheese", "URL1", 1L,
                 2L, "14.12.2021");
+        BestellFood bestellFoodInto2 = new BestellFood(2, 2, 10.0,
+                "Big Cheese", "Mit Cheese", "URL2", 1L,
+                1L, "14.12.2021");
+        BestellFood bestellFoodInto3 = new BestellFood(3, 3 ,10.0,
+                "Big Chaos", "Mit Cheese und Chili", "URL1", 3L,
+                2L, "14.12.2021");
+
+        //Essen in einer Bestellung aneinanderreihen
+        liste1.add(bestellFoodInto1);
+        liste1.add(bestellFoodInto2);
+        liste1.add(bestellFoodInto3);
+
+        //Bestellung in die Bestellhistorie hinzufuegen
+        //bestellungenList.add(liste1);
+
         System.out.println("Start");
     }
     @BeforeEach
@@ -37,21 +56,33 @@ class BestellHisRepoTest {
     @Test
     public void derTest1(){
         //Testfall 1:Prüfung ob Datum übereinstimmt.
-        System.out.println("Test1");
+        System.out.println("Test 1");
         assertEquals("14.12.2021",bestellFoodInto.getDate());
     }
     @Test
     public void derTest2(){
         //Testfall 2:Prüfung, ob Namen übereinstimmen.
+        System.out.println("Test 2");
+        assertEquals("Big Mac",bestellFoodInto.getName());
     }
 
     @Test
     public void derTest3(){
         //Testfall 3: Prüfung, ob bestellte Gerichte übereinstimmen.
+        System.out.println("Test 3");
+        //assertEquals();
     }
     @Test
     public void derTest4(){
         //Testfall 4:Prüfung, ob Summe übereinstimmt.
+        System.out.println("Test 4");
+        assertEquals(10.0,bestellFoodInto.getPreis());
+    }
+
+    @AfterEach
+    public void nach(){
+        System.out.println("nach Test");
+
     }
 
 
