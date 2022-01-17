9b28d967c8c0140708d91aedb00800f7cc7127ff
Leon Gashi <leon.gashi@stud.uni-due.de>
2022-01-17 16:37:44 +0100

    Aktionen Update

--- client/src/main/java/ude/sep/client/controller/AktionenController.java
+++ client/src/main/java/ude/sep/client/controller/AktionenController.java
@@ -1,6 +1,7 @@
 package ude.sep.client.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import org.json.JSONObject;
 
@@ -16,6 +17,16 @@ public class AktionenController extends ConnectionController {
 
     @FXML
     public void goSaleClick() throws IOException {
+
+        if(datum1.getText().equals("")) {
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Fehler!");
+            alert.setTitle("Bitte füllen Sie die Felder richtig aus!");
+            alert.setContentText("Bitte korrigieren!");
+            alert.showAndWait();
+        }
+
         JSONObject input = new JSONObject();
         input.put("rId" , LoginController.userId);
         input.put("start", datum1.getText());
