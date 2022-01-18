e25fa02f2cfc506a31fa3de5cdfa8c5e5b6d1e15
Oguzhan Kaluk <oguzhan.kaluk@stud.uni-due.de>
2022-01-18 17:42:36 +0100

    Lieferzeit bugfix

--- client/src/main/java/ude/sep/client/controller/ConnectionController.java
+++ client/src/main/java/ude/sep/client/controller/ConnectionController.java
@@ -146,7 +146,15 @@ public class ConnectionController {
         String url = "http://localhost:8080/time/findfor/"+timeFor;
         JSONArray allTimesFor = new JSONArray(JSONObjectGET(url).toString());
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        Duration output = Duration.between(LocalDateTime.now(), LocalDateTime.parse(allTimesFor.getJSONObject(0).getString("end"), dtf));
+        int index = 0;
+        for(int i=0;i<allTimesFor.length();i++) {
+            JSONObject cur = allTimesFor.getJSONObject(i);
+            JSONObject output = allTimesFor.getJSONObject(index);
+            if(LocalDate.parse(output.getString("start"), dtf).isBefore(LocalDate.parse(cur.getString("start"),dtf))) {
+                index = i;
+            }
+        }
+        Duration output = Duration.between(LocalDateTime.now(), LocalDateTime.parse(allTimesFor.getJSONObject(index).getString("end"), dtf));
         return (int) output.toMinutes();
     }
 
