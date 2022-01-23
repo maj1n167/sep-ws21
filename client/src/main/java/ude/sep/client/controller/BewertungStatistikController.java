581aed1fb02e4fae7da0022786c9f0185d5c199e
DursunBerat <dursun.saral@stud.uni-due.de>
2022-01-23 15:53:04 +0100

    update

--- client/src/main/java/ude/sep/client/controller/BewertungStatistikController.java
+++ client/src/main/java/ude/sep/client/controller/BewertungStatistikController.java
@@ -1,5 +1,6 @@
 package ude.sep.client.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.*;
 import org.json.JSONArray;
@@ -25,72 +26,62 @@ public class BewertungStatistikController extends ConnectionController {
     private int userId;
     LocalDate date= LoginController.date;
 
-
-
+    
     @FXML
     public void initialize() throws IOException {
         userId = LoginController.userId;
-        JSONArray ausgabe = new JSONArray();
-        JSONArray gBewertung = new JSONArray(JSONObjectGET("http://localhost:8080/rating").toString());
-        for (int i = 0; i < gBewertung.length(); i++) {
-            JSONObject curBewertung = gBewertung.getJSONObject(i);
-            if (curBewertung.get("restaurantId").equals(userId)) {
-                ausgabe.put(curBewertung);
-
-
-         }
-        }
-        System.out.println(ausgabe);
-
-        }
-
-
-
-
-    public void onBMessenTag() throws IOException {
-
+    }
+    public void letzteFünf() throws IOException {
         XYChart.Series series= new XYChart.Series();
-
-        series.getData().add(new XYChart.Data<>("1",10));
-        series.getData().add(new XYChart.Data<>("2",9));
-        series.getData().add(new XYChart.Data<>("3",9));
-        series.getData().add(new XYChart.Data<>("4",8));
-        series.getData().add(new XYChart.Data<>("5",9));
-        series.getData().add(new XYChart.Data<>("6",10));
-        series.getData().add(new XYChart.Data<>("7",9));
-
-        lineChartBewertungGesamt.getData().addAll(series);
-
-
-
-
-    }
-    public void onBMessenWoche() throws IOException {
-
-        XYChart.Series series= new XYChart.Series();
-
-        series.getData().add(new XYChart.Data<>("1",10));
-        series.getData().add(new XYChart.Data<>("2",9));
-        series.getData().add(new XYChart.Data<>("3",5));
-        series.getData().add(new XYChart.Data<>("4",4));
-        series.getData().add(new XYChart.Data<>("5",1));
-        series.getData().add(new XYChart.Data<>("6",10));
-        series.getData().add(new XYChart.Data<>("7",1));
-
-        lineChartBewertungGesamt.getData().addAll(series);
-
-
-
-    }
-    public void onBMessenMonat() throws IOException {
-
-
-
-    }
-
-
-
-        public void goBackControl() throws IOException {
-            changeScene("Startseite.fxml");
+        XYChart.Series series1= new XYChart.Series();
+        JSONArray gBewertung = new JSONArray(JSONObjectGET("http://localhost:8080/rating/"+userId+"/5").toString());
+        System.out.println(gBewertung);
+        lineChartBewertungGesamt.getData().clear();
+        series.getData().clear();
+        series1.getData().clear();
+        for (int i=0;i<gBewertung.length();i++) {
+            String letztenBewertungen = String.valueOf(i+1);
+                series.getData().add(new XYChart.Data<>(letztenBewertungen, gBewertung.getJSONObject(i).getInt("starsFood")));
+                series1.getData().add(new XYChart.Data<>(letztenBewertungen, gBewertung.getJSONObject(i).getInt("starsLieferung")));
         }
+        System.out.println(series);
+        lineChartBewertungGesamt.getData().addAll(series,series1);
     }
+
+    public void letzteZehn() throws IOException {
+        XYChart.Series series= new XYChart.Series();
+        XYChart.Series series1= new XYChart.Series();
+        JSONArray gBewertung = new JSONArray(JSONObjectGET("http://localhost:8080/rating/"+userId+"/10").toString());
+        System.out.println(gBewertung);
+        lineChartBewertungGesamt.getData().clear();
+        series.getData().clear();
+        series1.getData().clear();
+        for (int i=0;i<gBewertung.length();i++) {
+            String letztenBewertungen = String.valueOf(i+1);
+            series.getData().add(new XYChart.Data<>(letztenBewertungen, gBewertung.getJSONObject(i).getInt("starsFood")));
+            series1.getData().add(new XYChart.Data<>(letztenBewertungen, gBewertung.getJSONObject(i).getInt("starsLieferung")));
+        }
+        System.out.println(series);
+        lineChartBewertungGesamt.getData().addAll(series,series1);
+    }
+    public void letzteFünfzehn() throws IOException {
+        XYChart.Series series= new XYChart.Series();
+        XYChart.Series series1= new XYChart.Series();
+        JSONArray gBewertung = new JSONArray(JSONObjectGET("http://localhost:8080/rating/"+userId+"/15").toString());
+        System.out.println(gBewertung);
+        lineChartBewertungGesamt.getData().clear();
+        series.getData().clear();
+        series1.getData().clear();
+        for (int i=0;i<gBewertung.length();i++) {
+            String letztenBewertungen = String.valueOf(i+1);
+            series.getData().add(new XYChart.Data<>(letztenBewertungen, gBewertung.getJSONObject(i).getInt("starsFood")));
+            series1.getData().add(new XYChart.Data<>(letztenBewertungen, gBewertung.getJSONObject(i).getInt("starsLieferung")));
+        }
+        System.out.println(series);
+        lineChartBewertungGesamt.getData().addAll(series,series1);
+    }
+
+    public void zurückButton()throws IOException {
+        changeScene("BewertungList.fxml");
+    }
+}
