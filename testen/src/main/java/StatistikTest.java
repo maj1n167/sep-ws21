01a56cad0b4e357ee146a6c487c37288db3a90be
DursunBerat <dursun.saral@stud.uni-due.de>
2022-01-31 13:00:11 +0100

    update

--- testen/src/main/java/StatistikTest.java
+++ testen/src/main/java/StatistikTest.java
@@ -10,10 +10,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 public class StatistikTest extends ConnectionController {
-    public static JSONArray ball = new JSONArray();
+    public static JSONArray menu = new JSONArray();
+    public static JSONArray bestellung1 = new JSONArray();
+    public static JSONArray bestellung2 = new JSONArray();
     public static JSONObject b1 = new JSONObject();
     public static JSONObject b2 = new JSONObject();
     public static JSONObject b3 = new JSONObject();
@@ -22,97 +25,67 @@ public class StatistikTest extends ConnectionController {
 
     @BeforeAll
     public static void beforeAll() {
-
         //Speise hinzufügen
         b1.put("foodId", "1");
-        b1.put("menuId","1");
-        b1.put("preis","6.5");
         b1.put("name","Funghi");
-        b1.put("bild","");
-        b1.put("beschreibung","lecker");
-        b1.put("kategorieId", "1");
-
 
         //Speise hinzufügen
         b2.put("foodId", "2");
-        b2.put("menuId","1");
-        b2.put("preis","6.5");
         b2.put("name","Spaghetti");
-        b2.put("bild","");
-        b2.put("beschreibung","cremig");
-        b2.put("kategorieId", "2");
 
         //Speise hinzufügen
         b3.put("foodId", "3");
-        b3.put("menuId","1");
-        b3.put("preis","6.5");
         b3.put("name","Chicken Masala");
-        b3.put("bild","");
-        b3.put("beschreibung","sehr scharf");
-        b3.put("kategorieId", "3");
 
-        b3.put("foodId","2");
-        b3.put("name","Funghi");
+        // Speise hinzufügen
+        b4.put("foodId","4");
+        b4.put("name","Tonno");
 
-        // Bestellung 1
-        b4.put("date","30.01.2022");
-        b4.put("foodId","2");
-        b4.put("name","Funghi");
-    }
+        //Alle Speisen generieren
+        menu.put(b1);
+        menu.put(b2);
+        menu.put(b3);
+        menu.put(b4);
 
-
-
-    void initialize(int eingabe) {
-        JSONArray statistik = new JSONArray();
-        for (int i = 0;i < eingabe; i++) {
-            JSONObject day = new JSONObject();
-//            day.put("datum", LocalDate.now() - i);
-//            day.put("statistik", getAlleSpeisen(LocalDate.now() - i));
+        // bestellung 1 füllen
+        for (int i=0;i<5;i++){
+            bestellung1.put(b1);
+        }
+        for (int i=0;i<10;i++){
+            bestellung1.put(b2);
+        }
+        for (int i=0;i<15;i++){
+            bestellung1.put(b3);
+        }
+        for (int i=0;i<20;i++){
+            bestellung1.put(b4);
+        }
+        //bestellung 2 füllen
+        for (int i=0;i<5;i++){
+            bestellung2.put(b4);
+        }
+        for (int i=0;i<10;i++){
+            bestellung2.put(b3);
+        }
+        for (int i=0;i<15;i++){
+            bestellung2.put(b2);
+        }
+        for (int i=0;i<20;i++){
+            bestellung2.put(b1);
         }
     }
 
-
-
-    public JSONArray getAlleSpeisen(LocalDate datum) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        JSONArray alleSpeisen = new JSONArray(b3);
-        for (int i = 0; i < alleSpeisen.length(); i++) {
-            JSONObject toAdd = new JSONObject();
-            JSONObject curSpeise = alleSpeisen.getJSONObject(i);
-            toAdd.put("foodId", curSpeise.getInt("foodId"));
-            toAdd.put("count", 0);
-            alleSpeisen.put(toAdd);
-        }
-
-        JSONArray alleBestellungen = new JSONArray(b4);
-
-        for (int i = 0; i < alleBestellungen.length(); i++) {
-            JSONArray curBestellung = alleBestellungen.getJSONArray(i);
-            for (int j = 0; j < curBestellung.length(); j++) {
-                LocalDate curDate = LocalDate.parse(curBestellung.getJSONObject(j).getString("datum"), dtf);
-                JSONObject curFood = curBestellung.getJSONObject(j);
-                for (int k = 0; k < alleSpeisen.length(); k++) {
-                    if (alleSpeisen.getJSONObject(k).getInt("foodId") == curFood.getInt("foodId")
-                            && curDate.isEqual(datum)) {
-                        alleSpeisen.getJSONObject(k).put("count", alleSpeisen.getJSONObject(k).getInt("count") + 1);
-                    }
-                }
-            }
-        }
-        return alleSpeisen;
-    }
     @Test
     public  void getAlleSpeisenTest() throws IOException{
-        //ball.put(b1);
-        //int count= getAlleSpeisen();
-        //assertEquals(1,count);
-
-       // JSONObject input = new JSONObject();
-       // input.put("statistik", DateTimeFormatter.ofPattern("dd/MM/yyyy").format(LocalDateTime.now().minusDays(1)));
-        //int ergebnis = getAlleSpeisen(dateToString());
-       // assertEquals(4, ergebnis);
+        JSONArray statistik1= getStatistik(menu,bestellung1);
+        JSONArray statistik2= getStatistik(menu,bestellung2);
+        System.out.println(statistik1);
+        System.out.println(statistik2);
+        //assertEquals(5,statistik1.getJSONObject(0).getInt("count"));
 
 
+
+        System.out.println("getAlleSpeisenTest Test1: \nErwarteter Wert \"5\", berechneter Wert: "+statistik1.getJSONObject(0).getInt("count"));
     }
 
 }
